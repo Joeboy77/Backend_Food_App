e393d4aed76108bd002246ca20d937c3f96646f0
joeboy77 <acheampongjoseph470@gmail.com>
2024-10-08 17:15:22 +0000

    user model update ❤️

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express()
+const dbConnect = require('./src/db/dbConnect')
+const authRoutes = require('./src/routes/auth')
+const authMiddleware = require('./src/middleware/auth')
+
+app.use(express.json())
+app.use('/api/auth', authRoutes)
+app.use(authMiddleware)
+dbConnect()
+
+const port = 5000 
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+    
+})
--- src/models/User.js
+++ src/models/User.js
@@ -1,19 +1,32 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
+    username: {
         type: String,
         required: true,
         unique: true
     },
-    password: {
+    email: {
         type: String,
         required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    healthConditions: [String],
+    allergies: [String],
+    bloodGroup: String,
+    location: String
+})
+
+UserSchema.pre('save', async function name(next) {
+    if(this.isModified('password')) {
+        this.password = await bcrypt.hash(this.password, 8)
     }
+    next()
 })
 
 const User = mongoose.connect('User', UserSchema)
