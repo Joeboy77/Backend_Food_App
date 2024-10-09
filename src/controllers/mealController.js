c0a6ad57bd1644fced34c18c34bb6fd54b5d9f03
joeboy77 <acheampongjoseph470@gmail.com>
2024-10-09 09:34:21 +0000

    meal controller updated 🌹

--- src/controllers/mealController.js
+++ src/controllers/mealController.js
@@ -1,5 +1,6 @@
 const Meal = require('../models/mealModel')
 const Nutrition = require('../models/nutritionModel')
+const AIRecommendationService = require('../services/aiRecommendationService')
 
 exports.createMeal = async (req, res) => {
     try {
@@ -22,3 +23,28 @@ exports.getMeals = async (req, res) => {
         res.status(500).send(error)
     }
 }
+
+exports.getRecommendedMealPlan = async (req, res) => {
+    try {
+        const days = req.query.days ? parseInt(req.query.days) : 3;
+
+        const mealPlan = await AIRecommendationService.generateMealPlan(req.user._id, days)
+        res.send(mealPlan)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
+exports.getRecommendedMeal = async (req, res) => {
+    try {
+        const { mealType } = req.query
+        if(!['breakfast', 'lunch', 'dinner', 'snack'].includes(mealType)){
+            return res.status(400).send({ error: 'Invalid meal type' })
+        }
+        const meal = await AIRecommendationService.recommendMeal(req.user, mealType);
+        res.send(meal)
+    } catch (error) {
+        res.status(500).send(error)
+    }
+}
+
