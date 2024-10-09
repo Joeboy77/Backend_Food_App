54cbeca881e97f53f6d7d1a253bc932388666b41
joeboy77 <acheampongjoseph470@gmail.com>
2024-10-09 08:33:11 +0000

    middleware...

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -1,13 +1,23 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/User')
+const config = require('../config/config')
 
-const authenticate = async(req, res, next) => {
-    try{
-        const token = req.header('Authorization').replace('Bearer', '')
-        const decoded = jwt.verify(token, process.env.SECRET_KEY)
-        req.user = decoded;
+const authMiddleware = async (req, res, next) => {
+    try {
+        const token = req.header('Authorization').replace('Bearer ', '')
+        const decoded = jwt.verify(token, config.jwtSecret)
+        const user = await User.findOne({_id: decoded.userId})
+
+        if(!user) {
+            throw new Error()
+        }
+
+        req.token = token;
+        req.user = user
         next()
-    } catch(err) {
-        res.status(401).json({ message: 'Authentication required...'})
+    } catch (error) {
+        res.status(401).send({ error: 'Authentication required...'})
     }
 }
-module.exports = authenticate
+
+module.exports = authMiddleware
